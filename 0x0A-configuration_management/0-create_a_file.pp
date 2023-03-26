7da2cadac47b6a60036fc1c624e6a6639a5c7f7b
Your Nameroot@896cf839cf9a:/alx-pre_course# git add .root@896cf839cf9a:/alx-pre_course# git commit -m 'My first commit'[master (root-commit) 98eef93] My first commit1 file changed, 1 insertion(+)create mode 100644 README.mdroot@896cf839cf9a:/alx-pre_course# git pushEnumerating objects: 3, done.Counting objects: 100% (3/3), done.Writing objects: 100% (3/3), 212 bytes | 212.00 KiB/s, done.Total 3 (delta 0), reused 0 (delta 0)To https://github.com/Saddamzy001/alx-pre_course.git * [new branch]      master - mastegit config --global user.name Your <saddamhusseini1234@gmail.com>
2023-03-25 22:34:17 -0700

    Making correction

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-# this Puppet manifest creates a file in /tmp with the following attributes
-file {'/tmp/holberton':
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet',
-}
+# Create file in /tmp
+
+file { '/tmp/school' :
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    }
